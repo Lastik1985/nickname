c61a627f74cbbac293d6aca688b8bc9b75db746b
Lastik1985 <Lastik1985@mail.ru>
2023-01-30 01:25:01 +0300

    first

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,96 @@
+package org.example;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+    public static AtomicInteger counter3 = new AtomicInteger();
+    public static AtomicInteger counter4 = new AtomicInteger();
+    public static AtomicInteger counter5 = new AtomicInteger();
+
+    public static void main(String[] args) throws InterruptedException {
+        Random random = new Random();
+        String[] texts = new String[100_000];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("abc", 3 + random.nextInt(3));
+        }
+
+        long start = System.currentTimeMillis();
+
+        Thread[] threads = new Thread[3];
+        threads[0] = new Thread(() -> {
+            for (String text : texts) {
+                if (Palindrome(text) && !coincidence(text)) {
+                    Counter(text.length());
+                }
+            }
+        });
+        threads[1] = new Thread(() -> {
+            for (String text : texts) {
+                if (coincidence(text)) {
+                    Counter(text.length());
+                }
+            }
+        });
+        threads[2] = new Thread(() -> {
+            for (String text : texts) {
+                if (!Palindrome(text) && increasing(text)) {
+                    Counter(text.length());
+                }
+            }
+        });
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        System.out.println("Красивых слов с длиной 3: " + counter3 + " шт.");
+        System.out.println("Красивых слов с длиной 4: " + counter4 + " шт.");
+        System.out.println("Красивых слов с длиной 5: " + counter5 + " шт.");
+
+        long finish = System.currentTimeMillis();
+        System.out.println("Потребовалось " + (finish-start) + " сек.");
+    }
+
+    public static boolean Palindrome(String text) {
+        return text.equals(new StringBuilder(text).reverse().toString());
+    }
+
+    public static boolean coincidence(String text) {
+        for (int i = 1; i < text.length(); i++) {
+            if (text.charAt(i) != text.charAt(i - 1))
+                return false;
+        }
+        return true;
+    }
+
+    public static boolean increasing(String text) {
+        for (int i = 1; i < text.length(); i++) {
+            if (text.charAt(i) < text.charAt(i - 1))
+                return false;
+        }
+        return true;
+    }
+
+    public static void Counter(int textLength) {
+        if (textLength == 3) {
+            counter3.getAndIncrement();
+        } else if (textLength == 4) {
+            counter4.getAndIncrement();
+        } else {
+            counter5.getAndIncrement();
+        }
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
+
